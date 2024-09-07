0ef817aa25fc9e721f07b4672f2c7b532bb22ef5
KajalRai02 <kajal.rai@argusoft.in>
2024-09-07 09:17:52 +0000

    Convesion of datatype

---
+++ 03_datatypes/datatypeConversion.js
@@ -0,0 +1,29 @@
+let score =33
+let stringScore=String(score);
+
+console.log(typeof score)
+console.log(typeof stringScore)
+
+
+let a=""
+console.log(typeof a)  //string
+
+let b;
+console.log(typeof b)   //undefined
+
+
+let value1=null
+console.log(typeof value1)   //object
+
+let value2=NaN
+console.log(typeof value2)   //number
+
+let value3=undefined
+console.log(typeof value3)   //undefined
+
+
+let isAvailable=1;
+let check =  Boolean(isAvailable)
+
+console.log(typeof isAvailable)   //number
+console.log(typeof check)         // boolean
