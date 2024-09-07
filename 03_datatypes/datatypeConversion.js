1b1d44f71efa35eb0f41388ae80a1f0fd54d183f
KajalRai02 <kajal.rai@argusoft.in>
2024-09-07 12:47:13 +0000

    Data type and conversions

--- 03_datatypes/datatypeConversion.js
+++ 03_datatypes/datatypeConversion.js
@@ -27,3 +27,82 @@ let check =  Boolean(isAvailable)
 
 console.log(typeof isAvailable)   //number
 console.log(typeof check)         // boolean
+
+
+//************************************operations*****************************************
+
+console.log(1+2)            //3
+
+console.log(1+"2")          //12
+
+console.log("1"+2)          //12
+
+console.log(1+"2"+2)        //122
+
+console.log(1+2+"2")         //32
+
+console.log(+true)           //1
+
+
+//console.log(true+)  : gives error
+
+
+console.log(+"")//0
+
+
+
+// ******************************Comparisons*********************************************
+
+console.log("2"==2)  //returns true
+
+console.log(null > 0)  //false
+console.log(null<0)   //false
+console.log(null == 0)   //false
+console.log(null <= 0)   //true
+console.log(null >= 0)   //true
+
+// ===  : strict comparisons[it also compares datatype along with value]
+
+console.log("2"=== 2)  //false 
+
+
+
+//****************************************  Types of datatypes********************************
+
+//Also h=javascript is dynamically typed
+const s= "I am a string"
+console.log(typeof s)  //string  [ did not specify statically, so dynamically typed]
+
+const d=2
+console.log(typeof d)
+
+/*
+    primitive data type
+
+        number, bigInt, String, Boolean, Symbol, null, undefined
+
+    Non-primitive
+
+        array, objects, functions
+*/
+
+
+//Array [Internally. JS stores them in the form of objects, with key, value pair. and last key being length]
+const elements =["kajal", "shanti"]
+
+
+console.log(elements)
+console.log(typeof elements)    //objects
+console.log(elements.length)
+
+//objects[internally , js objects are hash map or hash table, they store key-value pair , 
+//        where keys are string or symbol and value can be of any datatype]
+
+
+const elements2 = {
+    name:"Vidhi",
+    age:24
+}
+
+console.log(elements2)
+console.log(typeof elements2)     //objects
---
+++ 04_memory/memory.js
@@ -0,0 +1,15 @@
+//Primitive - are stored in stack memory, and are passed by value
+
+let name="kajal"
+let anotherName = "rai"
+console.log(name)
+console.log(anotherName)
+
+
+//Non - Primitive are stored in heap memory, and are passed by referenced
+
+let object1=["kajal"]
+let object2=object1;
+object1[0]="Shanti"
+console.log(object1)
+console.log(object2)
